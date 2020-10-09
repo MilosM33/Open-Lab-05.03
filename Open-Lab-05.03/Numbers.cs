10844acc9cd70a2e35be564eeda915a947d4653a
MilosM33 <milosmarejka89@gmail.com>
2020-10-09 20:54:25 +0200

    init

--- Open-Lab-05.03/Numbers.cs
+++ Open-Lab-05.03/Numbers.cs
@@ -4,9 +4,8 @@ namespace Open_Lab_05._03
 {
     public class Numbers
     {
-        public string FormatNum(int num)
-        {
-            throw new NotImplementedException();
-        }
+        public string FormatNum(int num)=>num.ToString("N0").Replace(' ', ',');
+
+
     }
 }
